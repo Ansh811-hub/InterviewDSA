5ab4821c0b852c9940accf2c6d1ae3b8e6d37983
Ansh bhardwaj <www.anshbhardwaj1505@gmail.com>
2026-08-08 17:37:29 +0530

    binarysearch new question

---
+++ src/BinarySearch/MaximumCountOfPositiveandNegativeIntegers.java
@@ -0,0 +1,44 @@
+package BinarySearch;
+
+public class MaximumCountOfPositiveandNegativeIntegers {
+        public int maximumCount(int[] nums) {
+            int negative_num = findNegetive(nums);
+            int positive_num = nums.length - findPositive(nums);
+
+            return Math.max(negative_num, positive_num);
+        }
+
+        private int findNegetive(int[] nums) {
+            int lo = 0;
+            int hi = nums.length - 1;
+
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+
+                if (nums[mid] < 0) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+
+            return lo;
+        }
+
+        private int findPositive(int[] nums) {
+            int lo = 0;
+            int hi = nums.length - 1;
+
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+
+                if (nums[mid] <= 0) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+
+            return lo;
+        }
+    }
