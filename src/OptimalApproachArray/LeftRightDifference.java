0ae21907c23b1f1702ea7ea42ce3f982da2353e2
Ansh bhardwaj <www.anshbhardwaj1505@gmail.com>
2026-08-10 20:05:42 +0530

    mergeintervals

--- src/OptimalApproachArray/LeftRightDifference.java
+++ src/OptimalApproachArray/LeftRightDifference.java
@@ -6,21 +6,16 @@ public class LeftRightDifference {
         int[] ans = new int[n];
 
         int total = 0;
-
         for (int num : nums) {
             total += num;
         }
 
         int left = 0;
-
         for (int i = 0; i < n; i++) {
             int right = total - left - nums[i];
-
             ans[i] = Math.abs(left - right);
-
             left += nums[i];
         }
-
         return ans;
     }
 }
