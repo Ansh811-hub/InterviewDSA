2978df9aff847d2ec863676f09a98eae2d40aab7
Ansh bhardwaj <www.anshbhardwaj1505@gmail.com>
2026-08-30 01:35:25 +0530

    total number of words

--- src/Strings/MEDIUM/CountTotalNumberofWords.java
+++ src/Strings/MEDIUM/CountTotalNumberofWords.java
@@ -1,12 +1,26 @@
 package Strings.MEDIUM;
 
+import java.util.Scanner;
+
 public class CountTotalNumberofWords {
-    public static void valid(String s, char ch) {
-
-
+    public static void valid(String s) {
+        int count = 1;
+        for (int i = 0; i < s.length()-1; i++) {
+            char c = s.charAt(i);
+            char p = s.charAt(i+1);
+            if(c == ' ' && p != ' '){
+                count++;
+            }else{
+                continue;
+            }
+        }
+        System.out.println(count);
     }
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
+        valid(s);
     }
 
 }
